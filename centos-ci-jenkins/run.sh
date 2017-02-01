216d6defa42b62704eca6bd39c234f889fb8d533
Marek Skalický <mskalick@redhat.com>
2017-02-01 13:11:13 +0100

    Update run.sh to work better with two types of jobs (SCLo* and rhscl*) used in two different Jenkins instances.
    Update README.

--- centos-ci-jenkins/run.sh
+++ centos-ci-jenkins/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Install and run Jenkins Job Builder (JJB) and against ci.centos.org to update
-# job definitions for every software collection.
+# Install and run Jenkins Job Builder (JJB) and against Jenkins configured in jenkins_jobs.ini
+# and jenkins_jobs_rhscl.ini files.
 
 THISDIR=$(dirname ${BASH_SOURCE[0]})
 
@@ -22,10 +22,15 @@ if ! pip show jenkins-job-builder >/dev/null 2>&1; then
   pip install jenkins-job-builder
 fi
 
-# Generate a JJB config file
+# Generate a JJB config files
 if [ ! -f $THISDIR/jenkins_jobs.ini ]; then
   cp $THISDIR/jenkins_jobs.ini.template $THISDIR/jenkins_jobs.ini
   echo "jenkins_jobs.ini generated, please edit and add username/password to update ci.centos.org"
+  exit 1
+fi
+if [ ! -f $THISDIR/jenkins_jobs_rhscl.ini ]; then
+  cp $THISDIR/jenkins_jobs.ini.template $THISDIR/jenkins_jobs_rhscl.ini
+  echo "jenkins_jobs_rhscl.ini generated, please edit jenkins URL and add username/password to authenticate"
   exit 1
 fi
 
@@ -39,10 +44,32 @@ cat configuration | while read scl namespace gituser gitproject trigger hub_name
 done
 
 # Pass all other arguments through to JJB
-if [ $# -gt 0 ]; then
+if [ $# -eq 1 ]; then
   action=$1
   shift
-  jenkins-jobs --conf $THISDIR/jenkins_jobs.ini $action -r $THISDIR/yaml $*
+  [[ "${action}" == "update" ]] || [[ "${action}" == "test" ]] \
+    || [[ "${action}" == "delete" ]] || (jenkins-jobs ${action} $* && exit)
+  echo "Updating 'SCLo*' jobs ..."
+  jenkins-jobs --conf $THISDIR/jenkins_jobs.ini $action -r $THISDIR/yaml SCLo\* $*
+  echo "Updating 'rhscl*' jobs ..."
+  jenkins-jobs --conf $THISDIR/jenkins_jobs_rhscl.ini $action -r $THISDIR/yaml rhscl\* $*
+elif [ $# -eq 2 ]; then
+  action=$1
+  shift
+  [[ "${action}" == "update" ]] || [[ "${action}" == "test" ]] \
+    || [[ "${action}" == "delete" ]] || (jenkins-jobs ${action} $* && exit)
+  name=$1
+  shift
+  if [[ "${name}" == SCLo* ]]; then
+    echo "Updating '${name}' jobs ..."
+    jenkins-jobs --conf $THISDIR/jenkins_jobs.ini $action -r $THISDIR/yaml ${name} $*
+  elif [[ "${name}" == rhscl* ]]; then
+    echo "Updating '${name}' jobs ..."
+    jenkins-jobs --conf $THISDIR/jenkins_jobs_rhscl.ini $action -r $THISDIR/yaml ${name} $*
+  else
+    echo "WARNING: Please specify 'SCLo' of 'rhscl' prefix for job names to choose right JJB configuration file. Passing all arguments directly to jenkins-jobs command"
+    jenkins-jobs ${action} ${name} $*
+  fi
 else
   jenkins-jobs $*
 fi
